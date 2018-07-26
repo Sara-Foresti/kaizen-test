78712e9d86a9f012a46683338c0869fd60f65d82
Sara Foresti <Sara.foresti@hotmail.com>
2018-07-26 17:49:08 +0100

    Show the page after 2 seconds

---
+++ question1.js
@@ -0,0 +1,15 @@
+// Show the page after 2 seconds
+const page = document.querySelector("body");
+page.style.display = "none";
+
+const showPage = () => {
+  page.style.display = "";
+}
+
+setTimeout(showPage, 2000);
+
+// Add an additional article to the 4 that are in place currently
+
+
+
+
