fb018fdb65fa245d11229d862a99c0062e09e998
Sara Foresti <Sara.foresti@hotmail.com>
2018-07-26 18:06:35 +0100

    Add an additional article to the 4 that are in place currently

--- question1.js
+++ question1.js
@@ -9,7 +9,15 @@ const showPage = () => {
 setTimeout(showPage, 2000);
 
 // Add an additional article to the 4 that are in place currently
-
+const articles = document.querySelector(".row");
+articles.insertAdjacentHTML("beforeend",
+  `<article class="col-md-4">
+    <img src="https://hokcx-54c2.kxcdn.com/assets/recruitment/img/ultimatespidermanmarkbagley-56e20cd25f9b5854a9f8a199.jpg" />
+    <div class="col-md-12 banner">
+      <h3>Less Important Article</h3>
+      <p>This is another Spiderman article!</p>
+    </div>
+   </article>`);
 
 
 
